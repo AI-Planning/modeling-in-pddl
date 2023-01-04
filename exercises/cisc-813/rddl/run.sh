6717055ae905ed3bb1d69325d40bd1bd2863fb95
Christian Muise <christian.muise@gmail.com>
2023-01-04 15:37:38 -0500

    Giving up on stand-alone rddl+prost and will go with docker env.

--- exercises/cisc-813/rddl/run.sh
+++ exercises/cisc-813/rddl/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 echo "Starting RDDL Gym Server..."
-# ( cd /workspace/pyRDDLGym && python run.py $@ ) > pyrddl.log 2>&1 &
-( cd /workspace/pyRDDLGym && python run.py $@ ) &
+( cd /workspace/pyRDDLGym && python run.py $@ ) > pyrddl.log 2>&1 &
 
+sleep 5
 echo "Starting PROST..."
-# ( cd /workspace/prost && ./prost.py 813rocks "[PROST -s 1 -se [IPC2014]]" ) > prost.log 2>&1
-( cd /workspace/prost && ./prost.py 813rocks "[PROST -s 1 -se [IPC2014]]" )
+( cd /workspace/prost && ./search-release 813rocks "[PROST -s 1 -se [IPC2014]]" ) > prost.log 2>&1
 
 echo "Writing data.json..."
 cat /workspace/data.json > data.json
