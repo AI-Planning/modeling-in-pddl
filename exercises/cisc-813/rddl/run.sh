958fae72199e1394322716e54195826c49397e1a
Christian Muise <christian.muise@gmail.com>
2023-01-05 14:53:01 -0500

    Done fixing up the rddl+prost interface to be run.

--- exercises/cisc-813/rddl/run.sh
+++ exercises/cisc-813/rddl/run.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
+# Confirm 4 parameters
+if [ $# -ne 4 ]; then
+    echo
+    echo "  Usage: rddlprost <domain> <instance> <rounds (e.g., 30)> <time (e.g., 300)>"
+    echo
+    exit 1
+fi
+
+convert_to_absolute_path() {
+    if [[ $1 != /* ]]; then
+        ABS_PATH="$(pwd)/$(basename "$1")"
+    else
+        ABS_PATH=$1
+    fi
+    echo "$ABS_PATH"
+}
+
+DOM="$(convert_to_absolute_path "$1")"
+INST="$(convert_to_absolute_path "$2")"
+
+# Make sure they exist
+if [ ! -f "$DOM" ]; then
+    echo "Domain file $DOM does not exist."
+    exit 1
+fi
+
+if [ ! -f "$INST" ]; then
+    echo "Instance file $INST does not exist."
+    exit 1
+fi
+
 echo "Starting RDDL Gym Server..."
-( cd /workspace/pyRDDLGym && python run.py $@ ) > pyrddl.log 2>&1 &
+( cd /workspace/pyRDDLGym && python run.py "$DOM" "$INST" "$3" "$4" ) > pyrddl.log 2>&1 &
 
 sleep 5
 echo "Starting PROST..."
