0718129dd84bc4f6402aad6084374d1989a97128
Christian Muise <christian.muise@gmail.com>
2023-01-10 10:50:01 -0500

    Print the actions executed for the traces.

--- exercises/cisc-813/rddl/viz.py
+++ exercises/cisc-813/rddl/viz.py
@@ -11,20 +11,38 @@ DARKGREEN = '\x1b[6;30;48;5;28m'
 
 def main(data):
     traces = []
+    plans = []
     for trace in data:
         traces.append([])
+        plans.append([])
         for step in trace:
+            print(step)
             traces[-1].append(step['state'])
+            if step['actions']:
+                plans[-1].append(', '.join(step['actions']))
+            else:
+                plans[-1].append('')
 
-    for trace in traces:
-        print_trace(trace)
+    for trace, plan in zip(traces, plans):
+        print_trace(trace, plan)
 
-def print_trace(trace):
+def print_trace(trace, plan):
 
     fluents = list(sorted(trace[0].keys()))
     maxfluentlen = max(len(f) for f in fluents)+2
 
+    print("\nActions:")
+    for i in range(len(plan)):
+        print(str(i).rjust(2) + ': ' + str(plan[i]))
+
+    print("\nTrace:\n")
+
+    # Print the step numbers
+    print(' '*maxfluentlen, end=' ')
+    for i in range(len(trace)):
+        print(str(i).rjust(2), end=' ')
     print()
+
     for f in  fluents:
         # print the fluent right aligned with maxfluentlen characters
         print(f.rjust(maxfluentlen), end=' ')
@@ -43,6 +61,8 @@ def print_trace(trace):
                     print(DARKRED + '  ' + '\x1b[0m', end=' ')
         print()
     print()
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
