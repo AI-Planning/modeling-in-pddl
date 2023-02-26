47210b99dc0f0994860feba87c28f3b7dfd541e0
Christian Muise <christian.muise@gmail.com>
2023-02-25 23:35:46 -0500

    Major rewrite of the make-music script. Ready to rock!

--- exercises/cisc-813/temporal/make-music.py
+++ exercises/cisc-813/temporal/make-music.py
@@ -62,53 +62,77 @@ def main(fn):
 
     chords = []
     bars = []
-    now = 0.0
-    straddle = False
-    print("time\tnow\tline")
+
+    # Check if we dond't have any chords
+    if "play-chord" not in ''.join(lines):
+        chords.append((0.0, ""))
+        bars.append([])
+
+    # print("Time\tLine")
     for line in lines:
+
         if "progress" in line or line[0] == ';':
             continue
-        time = round(round(float(line.split(":")[0])*10) / 10.0, 1)
-        gap = time - now
-        if gap > 0:
-            bars[-1].append((now, "z", GAP_CONVERSION[gap]))
-            now = time + gap
-            print(f"\n  Gap: {gap} / now: {now}\n")
 
-        print(f"{time}\t{now}\t{line.strip()}")
+        time = round(round(float(line.split(":")[0])*10) / 10.0, 1)
+
+        # print(f"{time}\t{line.strip()}")
+
         if "play-chord" in line:
             assert time in [0, 4, 8, 12]
             chord = line.split()[2][:-1]
             chords.append((time, CHORD_CONVERSION[chord]))
-            if straddle:
-                straddle = False
-            else:
-                bars.append([])
-        elif "play-note" in line or "play-tonic-note" in line:
+            bars.append([])
+        elif "play-note" in line or "play-tonic-note" in line or "play-cheat-note" in line:
             note = line.split()[2]
             dur = line.split()[3]
+            # Fix for cheat notes
+            if dur[-1] == ')':
+                dur = dur[:-1]
             bars[-1].append((time, NOTE_CONVERSION[note], DURATION_CONVERSION[dur]))
-            now += DURATION_CONVERSION[dur] / 2
-        elif "play-cheat-note" in line:
-            note = line.split()[2]
-            dur = line.split()[3][:-1]
 
-            # Check if we should straddle the bar line
-            endpoint = time + DURATION_CONVERSION[dur] / 2
-            if bars[-1]:
-                barstart = bars[-1][0][0]
-            else:
-                barstart = endpoint
-            if (endpoint - barstart) > 4.0:
-                first_dur = 2*(4 - (time - barstart))
-                second_dur = 2*(endpoint - barstart - 4)
-                bars[-1].append((time, NOTE_CONVERSION[note], f"{int(first_dur)}-"))
-                bars.append([])
-                bars[-1].append((time+(first_dur/2), NOTE_CONVERSION[note], int(second_dur)))
-                straddle = True
-            else:
-                bars[-1].append((time, NOTE_CONVERSION[note], DURATION_CONVERSION[dur]))
-            now += DURATION_CONVERSION[dur] / 2
+    if len(chords) > 1:
+        # Maximum duration of the final note is capped to the last chord time + 4.0
+        max_time = chords[-1][0] + 4.0
+        final_start = bars[-1][-1][0]
+        final_duration = bars[-1][-1][2] / 2
+
+        if final_start + final_duration > max_time:
+            gap = GAP_CONVERSION[max_time - final_start]
+            bars[-1][-1] = (final_start, bars[-1][-1][1], gap)
+
+
+        # Next we want to go through and add straddle notes if necessary
+        for i in range(len(bars)-1):
+            max_time = chords[i][0] + 4.0
+            final_start = bars[i][-1][0]
+            final_duration = bars[i][-1][2] / 2
+            if final_start + final_duration > max_time:
+                extra = GAP_CONVERSION[(final_start + final_duration) - max_time]
+                gap = GAP_CONVERSION[max_time - final_start]
+                bars[i][-1] = (final_start, bars[i][-1][1], f"{gap}-")
+                bars[i+1].insert(0, (chords[i+1][0], bars[i][-1][1], extra))
+
+    # Finally, add any rests that are necessary
+    for i in range(len(bars)):
+        max_time = chords[i][0] + 4.0
+        now = chords[i][0]
+        j = -1
+        while j < len(bars[i])-1:
+            j += 1
+            note = bars[i][j]
+            if note[1] == "z":
+                continue
+            if note[0] > now:
+                gap = GAP_CONVERSION[note[0] - now]
+                bars[i] = bars[i][:j] + [(now, "z", gap)] + bars[i][j:]
+                j += 1
+            now = note[0] + note[2] / 2
+        # Final rests
+        if now < max_time:
+            gap = GAP_CONVERSION[max_time - now]
+            bars[i].append((now, "z", gap))
+
 
     for chord, events in zip(chords, bars):
         abc += chord[1] + "".join([note[1] + str(note[2]) for note in events]) + "|"
